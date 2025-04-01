c60cc49133b6d915c9a6498b01071bd703879679
devLavinea <lavinea2411@gmail.com>
2025-04-01 00:09:37 -0300

    favicon e indentação

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,113 +1,111 @@
-$(document).ready(function(){
-  $('#mobile_btn').on('click', function(){
-      $('#mobile_menu').toggleClass('active');
-      $('#mobile_btn').find('i').toggleClass('fa-x');
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
   });
 
-  const sections = $('section');
-  const navItems = $('.nav-item');
+  const sections = $("section");
+  const navItems = $(".nav-item");
 
-  $(window).on('scroll', function(){
-      const header = $('header');
-      const scrollPosition = $(window).scrollTop() - header.outerHeight();
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
-      let activeSectionIndex = 0;
+    let activeSectionIndex = 0;
 
-      if (scrollPosition <= 0){
-          header.css('box-shadow', 'none');
-      } else {
-          header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1)");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
       }
+    });
 
-      sections.each(function(i){
-          const section = $(this);
-          const sectionTop = section.offset().top - 96;
-          const sectionBottom = sectionTop + section.outerHeight();
-
-          if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
-              activeSectionIndex = i;
-              return false;
-          }
-      });
-
-      navItems.removeClass('active');
-      $(navItems[activeSectionIndex]).addClass('active');
+    navItems.removeClass("active");
+    $(navItems[activeSectionIndex]).addClass("active");
   });
 
-  ScrollReveal().reveal('.description', {
-      origin: 'right',
-      duration: 2000,
-      distance: '20%'
+  ScrollReveal().reveal(".description", {
+    origin: "right",
+    duration: 2000,
+    distance: "20%",
   });
 
-  ScrollReveal().reveal('.section-title, .title', {
-      origin: 'left',
-      duration: 2000,
-      distance: '20%'
+  ScrollReveal().reveal(".section-title, .title", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
   });
 
-  ScrollReveal().reveal('.text-car, .footer-text', {
-      origin: 'top',
-      duration: 2000,
-      distance: '20%'
+  ScrollReveal().reveal(".text-car, .footer-text", {
+    origin: "top",
+    duration: 2000,
+    distance: "20%",
   });
 
-  ScrollReveal().reveal('.service p, .card-testimonials p', {
-      origin: 'bottom',
-      duration: 2000,
-      distance: '20%'
+  ScrollReveal().reveal(".service p, .card-testimonials p", {
+    origin: "bottom",
+    duration: 2000,
+    distance: "20%",
   });
 });
 
 var swiper1 = new Swiper(".mySwiper1", {
-    observer: true,
-    observeParents: true,
-  
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      
-      400: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      
-      600: {
-        slidesPerView: 2,
-        spaceBetween: 25,
-      },
-      
-      1000: {
-        slidesPerView: 3,
-        spaceBetween: 25,
-      },
-    },
-  });
-  
+  observer: true,
+  observeParents: true,
 
-  var swiper2 = new Swiper(".mySwiper2", {
-    observer: true,
-    observeParents: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 25,
     },
-    breakpoints: {
-      // when window width is >= 320px
-    
-      // when window width is >= 480px
-      499: {
-        slidesPerView: 2,
-        spaceBetween: 15,
-      },
-    
-      // when window width is >= 640px
-      1000: {
-        slidesPerView: 4,
-        spaceBetween: 15,
-      },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 25,
     },
-  });
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  observer: true,
+  observeParents: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+
+    // when window width is >= 480px
+    499: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+
+    // when window width is >= 640px
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+  },
+});
